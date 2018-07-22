986dd7c42b6e7e424dad9d0cbd69be251c591db4
Olexandr Rizhiy <orizhiy@e1r8p11.unit.ua>
2018-07-22 16:37:31 +0300

    done

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+)
+
+type Data struct {
+	IdI         string `json:"idi"`
+	Description string `json:"description"`
+	ImageURL    string `json:"image_source"`
+	ImageLink   string `json:"image_link"`
+}
+
+func main() {
+	router := gin.Default()
+
+	slackService := NewService()
+
+	router.GET("/slack/earnings", func(c *gin.Context) {
+
+		var data Data
+		body := c.Request.Body
+		b, e := ioutil.ReadAll(body)
+		if e != nil {
+			fmt.Println(e)
+		}
+
+		err := json.Unmarshal(b, &data)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		slackService.PostMessage(data)
+
+		c.String(200, "Success")
+
+	})
+
+	router.Run(fmt.Sprintf("http://%s:%s", slackService.Port, slackService.Host))
+}
---
+++ slack.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/nlopes/slack"
+	"fmt"
+	"time"
+)
+
+type TypeSlackService struct {
+	Host   string
+	Port   string
+	Config *TypeConfig
+	Logger *TypeLogger
+	Client *slack.Client
+}
+
+func NewService() *TypeSlackService {
+	s := &TypeSlackService{}
+
+	s.Logger = NewLogger("approved-slack-service")
+	s.Config = s.LoadConfig()
+	s.Host, s.Port = s.GetSevicePath()
+	s.Client = slack.New("YOUR_TOKEN_HERE")
+	s.Logger.Info.Println("Create slack Client...")
+
+	return s
+}
+
+func NewAttachment(title string, imageURL string) []slack.Attachment {
+	return []slack.Attachment{
+		slack.Attachment{
+			Title:    title,
+			ImageURL: imageURL,
+		},
+	}
+}
+
+func NewMessage(idi string, imageLink string) string {
+	return fmt.Sprintf("Date: %s\nID: %s",
+		time.Now().Format("2006-01-02"),
+		fmt.Sprintf("<%s|%s>", imageLink, idi))
+}
+
+func (s *TypeSlackService) PostMessage(data Data) {
+	// If you set debugging, it will log all requests to the console
+	// Useful when encountering issues
+	s.Client.SetDebug(true)
+
+	message := NewMessage(data.IdI, data.ImageLink)
+
+	params := slack.PostMessageParameters{}
+	params.Attachments = NewAttachment(data.Description, data.ImageURL)
+
+	channelID, timestamp, err := s.Client.PostMessage(s.Config.Channel, message, params)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+}
